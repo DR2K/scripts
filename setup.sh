3e631989989017284411ea5951436f639f783191
Devdeep Rajpal <devdeeprajpal2k@outlook.com>
2020-11-24 00:07:09 -0600

    updated setup script-it almost works now!

--- setup.sh
+++ setup.sh
@@ -4,8 +4,28 @@ cd ~
 
 set -k
 
+#Xcode command line tools
+xcode-select --install
+
+#Pause b/c xcode lets things run
+/bin/bash read -n 1 -s -r -p "Press any key to continue AFTER xcode installs"
+
+#Set up Repos
+mkdir ~/Projects/
+cd ~/Projects/
+git clone https://github.com/DR2K/dotfiles.git
+git clone https://github.com/DR2K/scripts.git
+cd ~/
+
+for i in ~/Projects/dotfiles/.*
+do
+ln -s "$i" "${i##*/}"
+done
+
+rm ~/.git
+
 #Install ohMyZsh
-curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #ZshSyntaxHighlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -14,8 +34,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 #find zsh plugins in .zshrc and add them
-wowbutagain=$(grep -n "plugins=" .zshrc | cut -d : -f 2 | sed '1d')
-sed -i '' -e "s/$wowbutagain/plugins=(zsh-syntax-highlighting zsh-autosuggestions)/" .zshrc
+#wowbutagain=$(grep -n "plugins=" .zshrc | cut -d : -f 2 | sed '1d')
+#sed -i '' -e "s/$wowbutagain/plugins=(zsh-syntax-highlighting zsh-autosuggestions)/" .zshrc
 
 #FuzzyFinder
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
@@ -24,24 +44,21 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 #find zsh theme and change to p10k
-wow=$(grep -n 'ZSH_THEME="' .zshrc | cut -d : -f 2);
-sed -i '' -e "s#$wow#ZSH_THEME=\"powerlevel10k/powerlevel10k\"#" .zshrc
+#wow=$(grep -n 'ZSH_THEME="' .zshrc | cut -d : -f 2);
+#sed -i '' -e "s#$wow#ZSH_THEME=\"powerlevel10k/powerlevel10k\"#" .zshrc
 
 #brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 #Install Color LS
-gem install colorls
+sudo gem install colorls
 
-#Set up dotfile Repo
-mkdir ~/Projects/dotfiles
-cd ~/Projects/dotfiles
-git clone git@github.com:DR2K/dotfiles.git
-ln -s .* ~/
-rm -r ~/.git
-ln -s Brewfile
 #Install brew software
+cd ~/Projects/dotfiles
 brew bundle
 
-#restart shell
-exec $SHELL
+#reset Zsh
+mv .zshrc.pre-oh-my-zsh .zshrc
+
+#install fonts
+p10k configure
